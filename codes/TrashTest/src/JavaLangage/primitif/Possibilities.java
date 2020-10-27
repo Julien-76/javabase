4ac239fec80571983794a2a98fd66c2e8bc5ff8e
Julien-76 <julien.pothen@gmail.com>
2020-10-27 08:23:41 +0100

    27/10 début

--- codes/TrashTest/src/JavaLangage/primitif/Possibilities.java
+++ codes/TrashTest/src/JavaLangage/primitif/Possibilities.java
@@ -1,21 +1,54 @@
 package JavaLangage.primitif;
 
 public class Possibilities {
-    int data = 0b10010;
+    int data;
 
-    public boolean Countains(int nbre){
+    public boolean Countains(int nbreCountains){
         boolean place = true;
-        if ((this.data & (1 << (nbre-1))) != 0) {
+        if ((this.data & (1 << (nbreCountains - 1))) != 0) {
             place = false;
         }
         return place;
     }
 
-    public boolean Add(int nbre){
-        boolean place;
-        
-
-
+    public boolean Add(int nbreAdd){
+        boolean place = false;
+        if (this.Countains(nbreAdd)) {
+            this.data = this.data ^ (1 << (nbreAdd - 1));
+            place = true;
+        }
         return place;
     }
+
+    public boolean Remove(int nbreRemove){
+        boolean suppression = false;
+        if (!(this.Countains(nbreRemove))) {
+            this.data = this.data ^ (1 << (nbreRemove - 1));
+            suppression = true;
+        }
+        return suppression;
+    }
+
+    public int Size(){
+        int placesDispo = 9 - Integer.bitCount(this.data);
+
+
+        return placesDispo;
+    }
+
+    public static void main(String[] args) {
+        Possibilities testPoss = new Possibilities();
+        testPoss.data = 0b101110010;
+        System.out.println("Chanie de base : \n" + Integer.toBinaryString(testPoss.data));
+
+        System.out.println("\nAjouter position demandée : \n" + testPoss.Add(4));
+        System.out.println("Chaine après add : \n" + Integer.toBinaryString(testPoss.data));
+
+        System.out.println("\nRetirer position demandée : \n" + testPoss.Remove(8));
+        System.out.println("Chaine après remove : \n" + Integer.toBinaryString(testPoss.data));
+
+        System.out.println("\nPlaces restantes : \n" + testPoss.Size());
+    }
+
+
 }
